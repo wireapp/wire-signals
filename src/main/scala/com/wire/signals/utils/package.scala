2cd716e5289b290a4abf74d7bb5fced3fdec7eb6
makingthematrix <makingthematrix@gmail.com>
2020-11-21 22:50:01 +0100

    refactoring of 'returning'

--- src/main/scala/com/wire/signals/utils/package.scala
+++ src/main/scala/com/wire/signals/utils/package.scala
@@ -1,11 +1,8 @@
 package com.wire.signals
 
 package object utils {
-
-  private[signals] object returning {
-    @inline
-    final def apply[A](a: A)(body: A => Unit): A = {
-      body(a); a
-    }
+  @inline private[signals] def returning[A](a: A)(body: A => Unit): A = {
+    body(a)
+    a
   }
 }
